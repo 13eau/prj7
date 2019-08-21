f76e330a7099e0d4fa2c5797f9730060b46beb85
meng <527529798@qq.com>
2019-08-21 20:06:18 +0800

    增加了druid连接池 更正了数据库连接密码

--- src/test/java/com/woniuxy/Prj7ApplicationTests.java
+++ src/test/java/com/woniuxy/Prj7ApplicationTests.java
@@ -9,16 +9,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.woniuxy.entity.Party;
 import com.woniuxy.mapper.AssembleMapper;
+import com.woniuxy.mapper.PartyMapper;
 
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Prj7ApplicationTests {
+	@Resource
+	private PartyMapper partyMapper;
 	@Test
 	public void contextLoads() {
-		
+		Party party = partyMapper.selectByPrimaryKey(1);
+		System.out.println(party);
 	}
-
 }
